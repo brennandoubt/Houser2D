18dccb0b86f4b27bb8137dc6b8c2ce9ed86ac6c9
Brennan <brennandoubt@gmail.com>
2022-06-08 14:34:37 +0200

    Spawn code for homees

---
+++ Assets/Scripts/HomeeSpawner2D.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeeSpawner2D : MonoBehaviour
+{
+    public float spawnWidth = 1;
+    public float spawnRate = 1;
+    private float lastSpawnTime = 0;
+
+
+    public GameObject HomeePrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this is a simple timer structure that executes every 1/spawnRate seconds. This means it spawns spawnRate asteroids every second.
+        if (lastSpawnTime + 1 / spawnRate < Time.time)
+        {
+            lastSpawnTime = Time.time;
+            Vector3 spawnPosition = transform.position;
+            spawnPosition += new Vector3(Random.Range(-spawnWidth, spawnWidth), 0, 0);
+            // the Instatiate function creates a new GameObject copy (clone) from a Prefab at a specific location and orientation.
+            Instantiate(HomeePrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
