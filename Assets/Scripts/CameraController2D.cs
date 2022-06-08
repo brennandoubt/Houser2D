8175e982544f835e92b125249643ea0267e35de8
Brennan <brennandoubt@gmail.com>
2022-06-08 13:45:56 +0200

    Adding rest of files for project

---
+++ Assets/Scripts/CameraController2D.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Need camera to scale to aspect ratio
+ * 
+ * 
+ */
+public class CameraController2D : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
