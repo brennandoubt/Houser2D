26158ba59ab1788d65ba557de23515840a749a40
Brennan <brennandoubt@gmail.com>
2022-06-08 21:26:20 +0200

    Buttons now add/remove homees in scene

--- Assets/Scripts/ClickerController2D.cs
+++ Assets/Scripts/ClickerController2D.cs
@@ -6,24 +6,64 @@ using UnityEngine.UI;
 
 public class ClickerController2D : MonoBehaviour
 {
-    public int currHomees;
+    int currHomees;
+    protected bool hit;
+    protected bool minus;
+
+    public float spawnWidth = 1;
+    //public float spawnRate = 1;
+
+    public GameObject HomeePrefab;
+    public GameObject lastHomee;
+    //public GameObject nextHomee;
+
+    protected LinkedList<GameObject> HomeesInScene;
+    void Start()
+    {
+        HomeesInScene = new LinkedList<GameObject>();
+    }
+
     
     // spawning homee into scene on + click
     public void ClickPlus()
     {
         currHomees++;
         // TODO: add a homee to the scene (and maybe move into a house)
+        hit = true;
     }
     // removing homee from scene on - click
     public void ClickMinus()
     {
         currHomees--;
         // TODO: remove homee ("Person" prefab) from scene (a "home" in scene probably)
+        minus = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 spawnPos = transform.position;
+        spawnPos += new Vector3(Random.Range(-spawnWidth, spawnWidth), 0, 0);
+
+        if (hit)
+        {
+            GameObject newHomee = Instantiate(HomeePrefab, spawnPos, Quaternion.identity);
+            HomeesInScene.AddLast(newHomee);
+            //currHomees++;
+            lastHomee = HomeePrefab;
+            hit = false;
+        }
+        else if (minus)
+        {
+            if (HomeesInScene.Count > 0)
+            {
+                GameObject homeeToRemove = HomeesInScene.Last.Value; // last added to list is first removed (similar to stack data structure)
+                Destroy(homeeToRemove);
+
+                HomeesInScene.RemoveLast();
+            }
+            //currHomees--;
+            minus = false;
+        }
     }
 }
--- Assets/Scripts/HomeeSpawner2D.cs
+++ Assets/Scripts/HomeeSpawner2D.cs
@@ -6,15 +6,22 @@ public class HomeeSpawner2D : MonoBehaviour
 {
     public float spawnWidth = 1;
     public float spawnRate = 1;
-    private float lastSpawnTime = 0;
+    //private float lastSpawnTime = 0;
 
 
     public GameObject HomeePrefab;
 
+    private GameObject homees;
+
+    void Start()
+    {
+        homees = GetComponent<GameObject>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        // this is a simple timer structure that executes every 1/spawnRate seconds. This means it spawns spawnRate homees every second.
+        /*// this is a simple timer structure that executes every 1/spawnRate seconds. This means it spawns spawnRate homees every second.
         if (lastSpawnTime + 1 / spawnRate < Time.time)
         {
             lastSpawnTime = Time.time;
@@ -22,6 +29,13 @@ public class HomeeSpawner2D : MonoBehaviour
             spawnPosition += new Vector3(Random.Range(-spawnWidth, spawnWidth), 0, 0);
             // the Instatiate function creates a new GameObject copy (clone) from a Prefab at a specific location and orientation.
             Instantiate(HomeePrefab, spawnPosition, Quaternion.identity);
-        }
+        }*/
+
+        /*if (homees.OnClick())
+        {
+            Vector3 spawnPos = transform.position;
+            spawnPos += new Vector3(Random.Range(-spawnWidth, spawnWidth), 0, 0);
+            Instantiate(HomeePrefab, spawnPos, Quaternion.identity);
+        }*/
     }
 }
